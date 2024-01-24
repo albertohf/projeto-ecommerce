12be56e3d4f213fb0c942444b97084f41d85e83f
Albertohf <albertohf91@gmail.com>
2024-01-24 18:29:10 -0300

    autenticação com clerk

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
  return (
@@ -6,6 +7,19 @@ export default function Navbar() {
         <Link href="/" className="uppercase font-bold text-md flex items-center">
         Nc Rifas
         </Link>
+
+        <div className="flex items-center gap-8">
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <button className="uppercase rounded-md border border-gray-400  px-3 py-2 font-bold text-md h-12 flex items-center">
+                        Fazer Login
+                    </button>
+                </SignInButton>
+            </SignedOut>
+        </div>
     </nav>
  );
 }
